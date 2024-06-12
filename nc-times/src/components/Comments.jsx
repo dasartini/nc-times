9528cdeff869853e81248284b5c4f83fb530193b
dasartini <adriansartinid@gmail.com>
2024-06-12 14:21:18 +0100

    added icon and page title

--- nc-times/src/components/Comments.jsx
+++ nc-times/src/components/Comments.jsx
@@ -16,8 +16,6 @@ function Comments({ article_id, username ,comments, setComments}) {
     function handleDelete(commentId) {
         deleteCommentById(commentId)
             .then((data) => {
-                console.log("Comment deleted successfully:", data);
-                console.log(commentId)
                 setComments(comments.filter(comment => comment.comment_id !== commentId))
 
             })
@@ -35,7 +33,7 @@ function Comments({ article_id, username ,comments, setComments}) {
                             <h5>{comment.author} {comment.created_at === "just now"? "": "at"} {comment.created_at} {username === comment.author ? (<><button onClick={() => handleDelete(comment.comment_id)}>
                                 X</button></>) : (<></>)}</h5>
                             <p>{comment.body}</p>
-                            <button ><img className="like" src="https://i.pinimg.com/736x/8f/70/d4/8f70d42e7bd7d3a74e74e895b36293d4.jpg"></img></button> {comment.votes}<button><img className="like" src="https://i.imgflip.com/2qui05.jpg"></img></button>
+                            <button onClick={()=>{handleLike(1)}}><img className="like" src="https://i.pinimg.com/736x/8f/70/d4/8f70d42e7bd7d3a74e74e895b36293d4.jpg"></img></button> {comment.votes}<button><img className="like" src="https://i.imgflip.com/2qui05.jpg"></img></button>
                         </ul>
                     )
                 })}
